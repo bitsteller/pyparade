0db132490a04c14f542d1dec4e4a71ebb6976795
Nils Breyer <nbnet@arcor.de>
2015-10-05 09:36:59 +0200

    prepare for GroupByKey

--- pyparade/operations.py
+++ pyparade/operations.py
@@ -1,5 +1,22 @@
 import threading, multiprocessing, Queue, time
 
+def partition(mapped_values):
+	"""Organize the mapped values by their key.
+	Returns an unsorted sequence of tuples with a key and a sequence of values.
+
+	Args: 
+		mapped_values: a list of tuples containing key, value pairs
+
+	Returns:
+		A list of tuples (key, [list of values])
+	"""
+
+	partitioned_data = collections.defaultdict(list)
+	for key, value in mapped_values:
+		partitioned_data[key].append(value)
+	return partitioned_data.items()
+
+
 class Source(object):
 	def __init__(self):
 		super(Source, self).__init__()
@@ -16,7 +33,6 @@ class Source(object):
 	def stop(self):
 		if self.running.is_set():
 			self._stop_requested.set()
-
 
 class Operation(Source):
 	def __init__(self, source, num_workers=multiprocessing.cpu_count(), initializer = None):
@@ -64,6 +80,75 @@ class MapOperation(Operation):
 			else:
 				self.pool.terminate()
 				break
+
+		self.time_finished = time.time()
+		self.finished.set()
+		self.running.clear()
+
+class GroupByKeyOperation(Operation):
+	"""docstring for GroupByKeyOperation"""
+	def __init__(self, source, partly = False, num_workers=multiprocessing.cpu_count(), initializer = None):
+		super(GroupByKeyOperation, self).__init__(source, num_workers, initializer)
+		self.partly = partly
+		self.pool =  multiprocessing.Pool(num_workers, maxtasksperchild = 1000, initializer = initializer)
+
+	def __str__():
+		return "GroupByKey"
+
+	def run(self, chunksize=10):
+		self.running.set()
+		self.enqueued = 0
+		self.time_started = time.time()
+
+		#group
+		result = []
+		mapped = []
+		for v in self.inbuffer.generate():
+			if self.partly:
+				mapped.append(v)
+			else:
+				result.append(v)
+
+			if self._stop_requested.is_set():
+				self.time_finished = time.time()
+				self.finished.set()
+				self.running.clear()
+				return
+
+		if self.processed % (chunksize*self.num_workers) == 0:
+			#partition
+			partitioned_data = []
+			if self.partly:
+				partitioned_data = partition(mapped)
+			else:
+				partitioned_data = partition(result)
+
+			if self._stop_requested.is_set():
+				self.time_finished = time.time()
+				self.finished.set()
+				self.running.clear()
+				return
+
+			if partly:
+				mapped = []
+				for r in reduced:
+					self.outbuffer.put(r)
+			else:
+				result = reduced
+
+		#partition
+		partitioned_data = []
+		if self.partly:
+			partitioned_data = partition(mapped)
+		else:
+			partitioned_data = partition(result)
+
+		if partly:
+			mapped = []
+			for r in reduced:
+				self.outbuffer.put(r)
+		else:
+			result = reduced
 
 		self.time_finished = time.time()
 		self.finished.set()
