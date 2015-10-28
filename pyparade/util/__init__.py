0481a191d031eb59c700a1444aace9606205247c
Nils Breyer <nbnet@arcor.de>
2015-10-28 12:04:49 +0100

    use processes instead of threads for parallelisation

--- pyparade/util/__init__.py
+++ pyparade/util/__init__.py
@@ -1,6 +1,7 @@
 # coding=utf8
 import multiprocessing, time, math, traceback
-from threading import Event, Thread
+from multiprocessing import Process
+import multiprocessing
 
 def sstr(obj):
 	""" converts any object to str, if necessary encodes unicode chars """
@@ -16,7 +17,7 @@ class ParMap(object):
 		self.map_func = map_func
 		self.num_workers = num_workers
 		self.jobs = {}
-		self.request_stop = Event()
+		self.request_stop = multiprocessing.Event()
 		self.chunksize = 1
 
 	def stop(self):
@@ -28,13 +29,21 @@ class ParMap(object):
 		last_processing_times = []
 
 		for value in iterable:
-			#wait while all jobs are processing
-			while len(filter(lambda job: job["thread"].is_alive(), self.jobs.itervalues())) >= self.num_workers:
+			#wait as long as all jobs are processing
+			active_jobs = filter(lambda job: job["thread"].is_alive(), self.jobs.itervalues())
+			while len(active_jobs) >= self.num_workers:
 				time.sleep(0.1)
+				for job in active_jobs:
+					if job["connection"].poll():
+						job.update(job["connection"].recv())
+						active_jobs.remove(job)
 
 			#yield results while leftmost batch is ready
-			while len(self.jobs) > 0 and not self.jobs[min(self.jobs.iterkeys())]["thread"].is_alive(): 
+			while len(self.jobs) > 0 and (not self.jobs[min(self.jobs.iterkeys())]["thread"].is_alive() or self.jobs[min(self.jobs.iterkeys())]["connection"].poll()): 
 				minjobid = min(self.jobs.iterkeys())
+				if self.jobs[minjobid]["connection"].poll(0.5):
+					self.jobs[minjobid].update(self.jobs[minjobid]["connection"].recv())
+					self.jobs[minjobid]["connection"].close()
 
 				#update optimal chunksize based on 10*workers last batch processing times
 				while len(last_processing_times) > 10*self.num_workers:
@@ -44,7 +53,7 @@ class ParMap(object):
 				avg_processing_time = sum(last_processing_times)/len(last_processing_times)
 
 				slow_start_weight = 0.8 - 0.5*len(last_processing_times)/(10*self.num_workers) #update chunksize slowly in the beginning
-				desired_chunksize = int(math.ceil(slow_start_weight*self.chunksize + (1.0-slow_start_weight)*3/avg_processing_time)) #batch should take 3s to calculate
+				desired_chunksize = int(math.ceil(slow_start_weight*self.chunksize + (1.0-slow_start_weight)*3.0/avg_processing_time)) #batch should take 1s to calculate
 				self.chunksize = min(desired_chunksize, max(10,2*self.chunksize)) #double chunksize at most every time (but allow to go to 10 directly in the beginning)
 
 				if "error" in self.jobs[minjobid]:
@@ -64,7 +73,9 @@ class ParMap(object):
 				else:
 					job = {}
 					self.jobs[jobid] = job
-					job["thread"] = Thread(target = self._map_batch, args=(jobid, batch))
+					parent_conn, child_conn = multiprocessing.Pipe(duplex = False)
+					job["connection"] = parent_conn
+					job["thread"] = Process(target = self._map_batch, args=(child_conn, batch))#Thread(target = self._map_batch, args=(jobid, batch))
 					job["started"] = time.time()
 					job["thread"].start()
 
@@ -73,14 +84,20 @@ class ParMap(object):
 
 		job = {}
 		self.jobs[jobid] = job
-		job["thread"] = Thread(target = self._map_batch, args=(jobid, batch))
+		parent_conn, child_conn = multiprocessing.Pipe(duplex = False)
+		job["connection"] = parent_conn
+		job["thread"] = Process(target = self._map_batch, args=(child_conn, batch))# Thread(target = self._map_batch, args=(jobid, batch))
 		job["started"] = time.time()
-		job["thread"].run()
+		job["thread"].start()
 		batch = []
 
 		while len(self.jobs) > 0:
-			if not self.jobs[min(self.jobs.iterkeys())]["thread"].is_alive(): 
-				minjobid = min(self.jobs.iterkeys())
+			minjobid = min(self.jobs.iterkeys())
+			if not self.jobs[minjobid]["thread"].is_alive() or self.jobs[minjobid]["connection"].poll():
+				if self.jobs[minjobid]["connection"].poll():
+					self.jobs[minjobid].update(self.jobs[minjobid]["connection"].recv())
+					self.jobs[minjobid]["connection"].close()
+
 				if "error" in self.jobs[minjobid]:
 					raise self.jobs[minjobid]["error"]
 
@@ -88,20 +105,30 @@ class ParMap(object):
 					yield r
 				del self.jobs[minjobid]
 
-	def _map_batch(self, jobid, batch):
+	def _map_batch(self, conn, batch):
 		results = []
 		for value in batch:
 			if self.request_stop.is_set():
-				self.jobs[jobid]["error"] = StandardError("stop requested")
-				self.jobs[jobid]["stopped"] = time.time()
+				jobinfo = {}
+				jobinfo["error"] = StandardError("stop requested")
+				jobinfo["stopped"] = time.time()
+				conn.send(jobinfo)
+				conn.close()
 				return
 			try:
 				results.append(self.map_func(value))
 			except Exception, e:
 				traceback.print_stack()
 				print(e)
-				self.jobs[jobid]["error"] = e
-				self.jobs[jobid]["stopped"] = time.time()
+				jobinfo = {}
+				jobinfo["error"] = e
+				jobinfo["stopped"] = time.time()
+				conn.send(jobinfo)
+				conn.close()
 				return
-		self.jobs[jobid]["stopped"] = time.time()
-		self.jobs[jobid]["results"] = results
+
+		jobinfo = {}
+		jobinfo["stopped"] = time.time()
+		jobinfo["results"] = results
+		conn.send(jobinfo)
+		conn.close()
