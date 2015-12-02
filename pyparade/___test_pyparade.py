e97d1aa0df0b730089132270791b73851ab9de75
Nils Breyer <mail@nilsbreyer.eu>
2015-12-02 16:04:53 +0100

    fix unit test

--- pyparade/___test_pyparade.py
+++ pyparade/___test_pyparade.py
@@ -1,14 +1,16 @@
 # coding=utf-8
-import unittest, re
+import unittest, re, operator
+
+import pyparade
 
 class TestPyParade(unittest.TestCase):
 	"""Uses a comination of map and reduceByKey to calculate occurencies of each word in a text.
 	"""
 	def test_wordcount(self):
-		text = Dataset("abc test abc test test xyz")
-		words = text.map([(word, 1) for word in re.split(str, " ")])
-		counts = text.reduceByKey(add)
+		text = pyparade.Dataset(["abc test abc test test xyz", "abc test2 abc test cde xyz"])
+		words = text.flat_map(lambda line: [(word, 1) for word in re.split(" ", line)])
+		counts = words.reduce_by_key(operator.add)
 
-		p = ParallelProcess(counts, name="Counting words")
+		p = pyparade.ParallelProcess(counts, description="Counting words")
 		p.run()
-		print(p.collect())
+		print(counts.collect())
