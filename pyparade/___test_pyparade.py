11f06587817b22ddca1dc4e8a57d51393f4629f8
Nils Breyer <mail@nilsbreyer.eu>
2019-11-13 18:29:30 +0100

    fix

--- pyparade/___test_pyparade.py
+++ pyparade/___test_pyparade.py
@@ -9,8 +9,7 @@ class TestPyParade(unittest.TestCase):
 	def test_wordcount(self):
 		text = pyparade.Dataset(["abc test abc test test xyz", "abc test2 abc test cde xyz"])
 		words = text.flat_map(lambda line: [(word, 1) for word in re.split(" ", line)])
-		counts = words.reduce_by_key(operator.add)
+		wordcounts = words.reduce_by_key(operator.add)
 
-		p = pyparade.ParallelProcess(counts, description="Counting words")
-		p.run()
-		print(counts.collect())
+		result = wordcounts.collect(name="Counting words")
+		print(result)
--- test.py
+++ test.py
@@ -6,12 +6,12 @@ d = pyparade.Dataset(range(0,1000000), name="Numbers with a really extremly unne
 
 def f(a):
 	#print(str(a) + "->" + str(a+1))
-	time.sleep(0.001)
+	time.sleep(0.0001)
 	return a + 1
 
 def g(a):
 	#print(str(a) + "->" + str(a+1))
-	time.sleep(0.01)
+	time.sleep(0.001)
 	return a + 1
 
 inc = d.map(f, name="add 1", output_name="Numbers+1").map(g, name="add 1", output_name="Numbers+2").collect()
