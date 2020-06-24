f7a9a43d604f7abf0a41d2ff27287e679f07d081
nilbr50 <mail@nilsbreyer.eu>
2020-06-24 07:11:53 +0200

    Preparing release 0.4.6

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,4 @@
-0.4.6 (unreleased)
+0.4.6 (2020-06-24)
 ------------------
 
 - New: Optional debug mode for verbose output
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.4.6.dev0'
+version = '0.4.6'
 
 setup(name='pyparade',
       version=version,
